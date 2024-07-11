a6644d77119dcd75d64a72a2fcf12694e525491e
how-shaedy <168701405+how-shaedy@users.noreply.github.com>
2024-07-11 23:26:16 +0800

    Add files via upload

---
+++ Charles_and_Keith_404/Charles_and_Keith_404/script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('homeButton').addEventListener('click', function() {
+    window.location.href = 'https://www.charleskeith.com';
+});
+
+document.getElementById('contactButton').addEventListener('click', function() {
+    window.location.href = 'https://www.charleskeith.com/sg/contactus'; // Replace with the actual contact page URL
+});
